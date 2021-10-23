08cde3766515d2c274bcac9d42284335c84db97c
Sowmiya.B <61686122+sowmiya-cmd@users.noreply.github.com>
2021-10-23 19:25:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,261 @@
+
+<!DOCTYPE html>
+<html><style>
+body{
+}
+table,th,td{
+	border:2px solid black;
+width:1100px;
+background-color:lightgreen;
+}
+.btn{
+	width:50%;
+	height:5%;
+	font-size:22px;
+	padding:0px;
+}</style></body>
+
+<head> 
+    <meta charset="UTF-8">
+    <meta name="description" content="Loanday Template">
+    <meta name="keywords" content="Loanday, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Farm</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Offcanvas Menu Begin -->
+    <div class="offcanvas-menu-overlay"></div>
+    <div class="offcanvas-menu-wrapper">
+        <div class="offcanvas__search">
+            <i class="fa fa-search search-switch"></i>
+        </div>
+        <div class="offcanvas__logo">
+            <a href="./index.html"><img src="img/logo.png" alt=""></a>
+        </div>
+        <nav class="offcanvas__menu mobile-menu">
+            <ul>
+                <li class="active"><a href="./index.html">Home</a></li>
+              
+                <li><a href="./contact.html">Contact</a></li>
+            </ul>
+        </nav>
+        <div id="mobile-menu-wrap"></div>
+        <ul class="offcanvas__widget">
+            <li><i class="fa fa-map-marker"></i> Thiagarajar College Of Engineering,Madurai</li>
+            <li><i class="fa fa-phone"></i> +91 8838036610</li>
+            <li><i class="fa fa-envelope"></i> tceinfo@gmail.com</li>
+        </ul>
+        <div class="offcanvas__language">
+            <img src="img/flag.png" alt="">
+            <span>English</span>
+            <i class="fa fa-angle-down"></i>
+            <ul>
+                <li><a href="#">English</a></li>
+                <li><a href="#">Bangla</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Offcanvas Menu End -->
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="header__top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9">
+                        <ul class="header__top__widget">
+                            <li><i class="fa fa-map-marker"></i> Thiagarajar college of Engineering,Madurai</li>
+                            <li><i class="fa fa-phone"></i> +91 8838036610</li>
+                            <li><i class="fa fa-envelope"></i> tceinfo@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="header__top__language">
+                            <img src="img/flag.png" alt="">
+                            <span>English</span>
+                            <i class="fa fa-angle-down"></i>
+                            <ul>
+                                <li><a href="#">English</a></li>
+                                <li><a href="#">Bangla</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="header__logo">
+                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="header__nav">
+                        <nav class="header__menu">
+                            <ul>
+                                <li class="active"><a href="./index.html">Home</a></li>
+                             
+                                <li><a href="./contact.html">Contact</a></li>
+                            </ul>
+                        </nav>
+                        <div class="header__search">
+                            <i class="fa fa-search search-switch"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="canvas__open">
+                <span class="fa fa-bars"></span>
+            </div>
+        </div>
+    </header>
+    <!-- Header Section End -->
+
+    <!-- Hero Section Begin -->
+    <section class="hero set-bg" data-setbg="img/hero-bg.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="hero__text">
+                        <h2>Agriculture &Horticulture Corner,Madurai</h2>
+                        <a href="https://farmer.gov.in/" class="primary-btn">Get Start</a>
+                       </div>
+                </div>
+                <div class="col-lg-5 offset-lg-2">
+                    <div class="hero__form">
+					<div class="container">
+                        <h3>Search</h3>
+						<form action="" method="POST">
+                       <b> <input type="text" name="location" class="btn" placeholder="Location">
+						<br><input type ="submit" name="search" class="btn" value="Get Details Now"></b>
+                </div>
+            </div>
+        </div>
+    </section>  </form>
+<table>
+	<tr>
+	<th>location</th>
+	<th>fertility-level</th>
+	<th>suggested-crops</th>
+	<th>fertilizers</th></tr><br>
+	<?php
+	$connection=mysqli_connect("localhost","root","");
+	$db=mysqli_select_db($connection,'location');
+	if(isset($_POST['search']))
+	{
+	$location=$_POST['location'];
+	$result= mysqli_query($con,"SELECT location,fertility-level,suggested-crops,fertilizers FROM 'village' where location='$location' ")or die("Error:".mysqli_error($con));
+	$query_run=mysqli_query($connection,$result);
+	while($row=mysqli_fetch_array($query_run))
+	{
+	?>
+	<tr>
+	<td><?php echo $row['location'];?></td>
+	<td><?php echo $row['fertility-level'];?></td>
+	<td><?php echo $row['suggested-crops'];?></td>
+	<td><?php echo $row['fertilizers'];?></td>
+	</tr>
+	<?php
+	}
+	}?>
+	</table></div>
+
+           <br><br>     
+
+   
+  
+
+    <!-- Footer Section Begin -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__logo">
+                            <a href="./index.html"><img src="#" alt=""></a>
+                        </div>
+                        <p> <span>Contact Us Now!</span>
+                        <h2>+918838036610</h2>
+                    </div>
+                </div>
+                
+                <div class="col-lg-2 col-md-3 col-sm-6">
+                    <div class="footer__widget">
+                        <h5>Socials</h5>
+                        <div class="footer__widget footer__widget--social">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i> Facebook</a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i> Instagram</a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i> Twitter</a></li>
+                                <li><a href="#"><i class="fa fa-skype"></i> Skype</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+                    <div class="footer__widget footer__widget--address">
+                      
+                        <
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__copyright">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6">
+                        <ul class="footer__copyright__links">
+                            <li><a href="#">Terms of use</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Community</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <div class="footer__copyright__text">
+                            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |  <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                        </div>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+
+   
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body></body>
+
+</html> 
